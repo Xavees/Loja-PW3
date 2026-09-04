6467d889e73511422da579e89de574cf3c0ced77
unknown <LUIS_5295@UIRAPURU.BR>
2026-09-03 21:54:49 -0300

    add:

--- loja/sql/loja.sql
+++ loja/sql/loja.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE loja_db IF NOT EXISTS;
+CREATE DATABASE  IF NOT EXISTS loja_db;
 USE loja_db;
 
-CREATE TABLE produtos IF NOT EXISTS(
+CREATE TABLE  IF NOT EXISTS produtos (
     id_produto int AUTO_INCREMENT PRIMARY KEY,
     nome_produto varchar(100),
     descricao text,
     valor double
 );
 
-CREATE TABLE imagens IF NOT EXISTS(
+CREATE TABLE   IF NOT EXISTS imagens (
     id_imagem int AUTO_INCREMENT PRIMARY KEY,
     nome_imagem varchar(100),
     fk_id_produto int,
